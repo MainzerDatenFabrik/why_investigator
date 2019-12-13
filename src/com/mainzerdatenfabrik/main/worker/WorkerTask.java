05f814ebce59120e9bc157a94540d9c4dd4a8048
aPriesterroth <aaron@priesterroth.de>
2019-12-13 13:07:46 +0100

    Extend log message of "performCheck" method Rebuild artifact

--- src/com/mainzerdatenfabrik/main/worker/WorkerTask.java
+++ src/com/mainzerdatenfabrik/main/worker/WorkerTask.java
@@ -128,30 +128,33 @@ public class WorkerTask implements Runnable {
             return;
         }
 
+        String currentCheck = "";
+
         for(Check check : checks) {
             String query = check.getQuery(serverVersion);
+            currentCheck= check.getName();
 
             if(query != null) {
-                Logger.getLogger().info(Thread.currentThread().getName() + " executing check: " + check.getName() + ".");
+                Logger.getLogger().info(Thread.currentThread().getName() + " executing check: " + currentCheck + ".");
                 try {
                     Statement statement = connection.createStatement();
                     processQueryResults(statement.executeQuery(query), check, timestamp,
                             UtilsJDBC.NO_SPECIFIC_DATABASE);
                     statement.close();
                 } catch (SQLException e) {
-                    Logger.getLogger().warning("SQL Exception occurred while performing checks.");
+                    Logger.getLogger().warning("SQL Exception occurred while performing check " + currentCheck + ".");
                     Logger.getLogger().log(Level.SEVERE, e.getMessage(), e);
                     Slack.sendMessage("*SQLWorker*: SQL exception occurred: *" + e.getMessage() + "*.");
                 }
             } else {
-                Logger.getLogger().warning("Check " + check.getName() + " is not compatible with version " + serverVersion + "!");
+                Logger.getLogger().warning("Check " + currentCheck + " is not compatible with version " + serverVersion + "!");
             }
         }
 
         try {
             connection.close();
         } catch (SQLException e) {
-            Logger.getLogger().info("Exception occurred while performing check.");
+            Logger.getLogger().info("Exception occurred while performing check " + currentCheck + ".");
             Logger.getLogger().log(Level.SEVERE, e.getMessage(), e);
         }
     }
